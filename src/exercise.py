62f97bf1309280087ec110f4b24690bbd59865ef
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-06 11:10:23 +0100

    Attempt 1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
-    #write your code below this line
+        first = int(input('Give the first number:'))
+        second = int(input('Give the second number:'))#write your code below this line
+        final = first + second
+        print('The sum of the numbers is ' + str(final))#write your code below this line
 
 if __name__ == '__main__':
     main()
